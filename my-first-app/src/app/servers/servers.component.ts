302fa2b6b49d9fc18a6256633c14d35cff6954b2
Nagendra Prasad <nagendramca2011@gmail.com>
2018-05-18 20:13:56 +0530

    The Basics: Fully understanding the component selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `
+  // selector: '[app-servers]',
+  // selector:'.app-servers',
+  selector:'app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  templateUrl:'./servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+allowNewServer=false;
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000)
+   }
 
   ngOnInit() {
   }
